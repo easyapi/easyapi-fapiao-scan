b6c9f7a1ef454a359b300b5cc8f583bf41ff36fb
leida <zhang20084@126.com>
2023-03-20 00:56:14 +0800

    fix:items修改

--- js/index.js
+++ js/index.js
@@ -143,7 +143,7 @@ const appHtml = {
         }
         this.scanContent = res.data.content;
         this.accessToken = this.scanContent.accessToken;
-        this.scanList = res.data.content.scanItems;
+        this.scanList = res.data.content.items;
         this.invoiceForm.remark = this.scanContent.remark;
       }).catch(error => {
         console.log(error)
