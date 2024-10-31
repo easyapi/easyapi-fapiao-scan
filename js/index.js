d9bf0f62e31437d683fd4f98cb821b84b3aa9ae0
chenyunc123 <1537299563@qq.com>
2024-10-31 19:18:38 +0800

    头部标题设置

--- js/index.js
+++ js/index.js
@@ -28,8 +28,10 @@ const appHtml = {
       invoiceCategoryList: []
     }
   },
-  created() {
+  mounted() {
     document.title = "扫码开票";
+  },
+  created() {
     // 获取二维码的code
     this.code = getQueryString("code");
     this.getScan();
